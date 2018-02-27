e1c61fdd8a07731d0c14ffb86cce1b65fe3f7f62
Yulia <Yulia@yulias-mac.local>
2018-02-27 23:30:42 +0000

    Initial Commit

---
+++ bluetooth connection/PreferencesTableViewController.swift
@@ -0,0 +1,114 @@
+
+
+import UIKit
+
+final class PreferencesTableViewController: UITableViewController {
+    
+//MARK: Variables
+    
+    var selectedMessageOption: MessageOption!
+    var selectedReceivedMessageOption: ReceivedMessageOption!
+    var writeWithResponse = false
+
+
+//MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // get current prefs
+        selectedMessageOption = MessageOption(rawValue: NSUserDefaults.standardUserDefaults().integerForKey(MessageOptionKey))
+        selectedReceivedMessageOption = ReceivedMessageOption(rawValue: NSUserDefaults.standardUserDefaults().integerForKey(ReceivedMessageOptionKey))
+        writeWithResponse = NSUserDefaults.standardUserDefaults().boolForKey(WriteWithResponseKey)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+//MARK: UITableViewDelegate
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+       
+        // is it for the selectedMessageOption or for the selectedReceivedMessageOption? (section 0 or 1 resp.)
+        if indexPath.section == 0 {
+            
+            // first clear the old checkmark
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 0))?.accessoryType = .None
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: 0))?.accessoryType = .None
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 2, inSection: 0))?.accessoryType = .None
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 3, inSection: 0))?.accessoryType = .None
+            
+            // get the newly selected option
+            let selectedCell = indexPath.row
+            selectedMessageOption = MessageOption(rawValue: selectedCell)
+
+            // set new checkmark
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: selectedCell, inSection: 0))?.accessoryType = UITableViewCellAccessoryType.Checkmark
+            
+            // and finally .. save it
+            NSUserDefaults.standardUserDefaults().setInteger(selectedCell, forKey: MessageOptionKey)
+            
+        } else if indexPath.section == 1 {
+            
+            // first, clear the old checkmark
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 1))?.accessoryType = .None
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: 1))?.accessoryType = .None
+            
+            // get the newly selected option
+            let selectedCell = indexPath.row
+            selectedReceivedMessageOption = ReceivedMessageOption(rawValue: selectedCell)
+
+            // set new checkmark
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: selectedCell, inSection: 1))?.accessoryType = UITableViewCellAccessoryType.Checkmark
+            
+            // save it
+            NSUserDefaults.standardUserDefaults().setInteger(selectedCell, forKey: ReceivedMessageOptionKey)
+
+        } else if indexPath.section == 2 {
+            
+            // first, clear the old checkmark
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 0, inSection: 2))?.accessoryType = .None
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: 1, inSection: 2))?.accessoryType = .None
+            
+            // get the newly selected option
+            writeWithResponse = indexPath.row == 0 ? false : true
+            
+            // set new checkmark
+            tableView.cellForRowAtIndexPath(NSIndexPath(forRow: indexPath.row, inSection: 2))?.accessoryType = UITableViewCellAccessoryType.Checkmark
+            
+            // save it
+            NSUserDefaults.standardUserDefaults().setBool(writeWithResponse, forKey: WriteWithResponseKey)
+            
+            // set it
+            serial.writeType = writeWithResponse ? .WithResponse : .WithoutResponse
+
+        }
+        
+        // deselect row
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        // prevent the checkmarks from disappearing when they are scrolled out of screen and then back in
+        if indexPath.section == 0 && indexPath.row == selectedMessageOption.rawValue {
+            cell.accessoryType = .Checkmark
+        } else  if indexPath.section == 1 && indexPath.row == selectedReceivedMessageOption.rawValue {
+            cell.accessoryType = .Checkmark
+        } else if indexPath.section == 2 && indexPath.row == Int(writeWithResponse) {
+            cell.accessoryType = .Checkmark
+        }
+    }
+    
+    
+//MARK: IBActions
+
+    @IBAction func done(sender: AnyObject) {
+        // dismiss
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+}
---
+++ bluetooth connection/SerialViewController.swift
@@ -0,0 +1,196 @@
+
+import UIKit
+import CoreBluetooth
+import QuartzCore
+
+/// The option to add a \n or \r or \r\n to the end of the send message
+enum MessageOption: Int {
+    case NoLineEnding,
+         Newline,
+         CarriageReturn,
+         CarriageReturnAndNewline
+}
+
+/// The option to add a \n to the end of the received message (to make it more readable)
+enum ReceivedMessageOption: Int {
+    case None,
+         Newline
+}
+
+final class SerialViewController: UIViewController, UITextFieldDelegate, BluetoothSerialDelegate {
+
+//MARK: IBOutlets
+    
+    @IBOutlet weak var mainTextView: UITextView!
+    @IBOutlet weak var messageField: UITextField!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint! // used to move the textField up when the keyboard is present
+    @IBOutlet weak var barButton: UIBarButtonItem!
+    @IBOutlet weak var navItem: UINavigationItem!
+
+//MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // init serial
+        serial = BluetoothSerial(delegate: self)
+        serial.writeType = NSUserDefaults.standardUserDefaults().boolForKey(WriteWithResponseKey) ? .WithResponse : .WithoutResponse
+        
+        // UI
+        mainTextView.text = ""
+        reloadView()
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SerialViewController.reloadView), name: "reloadStartViewController", object: nil)
+        
+        // we want to be notified when the keyboard is shown (so we can move the textField up)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SerialViewController.keyboardWillShow(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(SerialViewController.keyboardWillHide(_:)), name: UIKeyboardWillHideNotification, object: nil)
+        
+        // to dismiss the keyboard if the user taps outside the textField while editing
+        let tap = UITapGestureRecognizer(target: self, action: #selector(SerialViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+        
+        // style the bottom UIView
+        bottomView.layer.masksToBounds = false
+        bottomView.layer.shadowOffset = CGSizeMake(0, -1)
+        bottomView.layer.shadowRadius = 0
+        bottomView.layer.shadowOpacity = 0.5
+        bottomView.layer.shadowColor = UIColor.grayColor().CGColor
+    }
+
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        // animate the text field to stay above the keyboard
+        var info = notification.userInfo!
+        let value = info[UIKeyboardFrameEndUserInfoKey] as! NSValue
+        let keyboardFrame = value.CGRectValue()
+        
+        //TODO: Not animating properly
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.bottomConstraint.constant = keyboardFrame.size.height
+            }, completion: { Bool -> Void in
+            self.textViewScrollToBottom()
+        })
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        // bring the text field back down..
+        UIView.animateWithDuration(1, delay: 0, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+            self.bottomConstraint.constant = 0
+        }, completion: nil)
+
+    }
+    
+    func reloadView() {
+        // in case we're the visible view again
+        serial.delegate = self
+        
+        if serial.isReady {
+            navItem.title = serial.connectedPeripheral!.name
+            barButton.title = "Disconnect"
+            barButton.tintColor = UIColor.redColor()
+            barButton.enabled = true
+        } else if serial.state == .PoweredOn {
+            navItem.title = "Bluetooth Serial"
+            barButton.title = "Connect"
+            barButton.tintColor = view.tintColor
+            barButton.enabled = true
+        } else {
+            navItem.title = "Bluetooth Serial"
+            barButton.title = "Connect"
+            barButton.tintColor = view.tintColor
+            barButton.enabled = false
+        }
+    }
+    
+    func textViewScrollToBottom() {
+        let range = NSMakeRange(NSString(string: mainTextView.text).length - 1, 1)
+        mainTextView.scrollRangeToVisible(range)
+    }
+    
+
+//MARK: BluetoothSerialDelegate
+    
+    func serialDidReceiveString(message: String) {
+        // add the received text to the textView, optionally with a line break at the end
+        mainTextView.text! += message
+        let pref = NSUserDefaults.standardUserDefaults().integerForKey(ReceivedMessageOptionKey)
+        if pref == ReceivedMessageOption.Newline.rawValue { mainTextView.text! += "\n" }
+        textViewScrollToBottom()
+        
+    }
+    
+    func serialDidDisconnect(peripheral: CBPeripheral, error: NSError?) {
+        reloadView()
+        dismissKeyboard()
+        let hud = MBProgressHUD.showHUDAddedTo(view, animated: true)
+        hud.mode = MBProgressHUDMode.Text
+        hud.labelText = "Disconnected"
+        hud.hide(true, afterDelay: 1.0)
+    }
+    
+    func serialDidChangeState(newState: CBCentralManagerState) {
+        reloadView()
+        if newState != .PoweredOn {
+            dismissKeyboard()
+            let hud = MBProgressHUD.showHUDAddedTo(view, animated: true)
+            hud.mode = MBProgressHUDMode.Text
+            hud.labelText = "Bluetooth turned off"
+            hud.hide(true, afterDelay: 1.0)
+        }
+    }
+    
+    
+//MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if !serial.isReady {
+            let alert = UIAlertController(title: "Not connected", message: "What am I supposed to send this to?", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: { action -> Void in self.dismissViewControllerAnimated(true, completion: nil) }))
+            presentViewController(alert, animated: true, completion: nil)
+            messageField.resignFirstResponder()
+            return true
+        }
+        
+        // send the message to the bluetooth device
+        // but first, add optionally a line break or carriage return (or both) to the message
+        let pref = NSUserDefaults.standardUserDefaults().integerForKey(MessageOptionKey)
+        var msg = messageField.text!
+        switch pref {
+        case MessageOption.Newline.rawValue:
+            msg += "\n"
+        case MessageOption.CarriageReturn.rawValue:
+            msg += "\r"
+        case MessageOption.CarriageReturnAndNewline.rawValue:
+            msg += "\r\n"
+        default:
+            msg += ""
+        }
+        
+        // send the message and clear the textfield
+        serial.sendMessageToDevice(msg)
+        messageField.text = ""
+        return true
+    }
+    
+    func dismissKeyboard() {
+        messageField.resignFirstResponder()
+    }
+    
+    
+//MARK: IBActions
+
+    @IBAction func barButtonPressed(sender: AnyObject) {
+        if serial.connectedPeripheral == nil {
+            performSegueWithIdentifier("ShowScanner", sender: self)
+        } else {
+            serial.disconnect()
+            reloadView()
+        }
+    }
+}
